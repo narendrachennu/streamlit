20b112f03ac12d976e1beea98a7eaf2a5fc9c2ec
narendrachennu <2631narendra@gmail.com>
2024-08-20 21:44:02 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+
+st.title('Test App')
+
+if st.button('Say Hello'):
+    st.write('Hello, Streamlit!')
+
+
+# In[ ]:
+
+
+
+
